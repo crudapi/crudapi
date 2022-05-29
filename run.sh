d37a87e8e5180235c6d53d5d6e6452c1e3078485
crudapi <admin@crudapi.cn>
2022-05-29 22:44:54 +0800

    sed password

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cp -R /crudapi/dist/* /crudapi/maven
 
 sed -i "s/server.port=.*$/server.port=$CRUDAPI_SERVER_PORT/g" application-prod.properties
 sed -i "s/spring.datasource.password=.*$/spring.datasource.password=$CRUDAPI_MYSQL_PASSWORD/g" application-prod.properties
-sed -i "s/spring.datasource.hikari.data-sources[0].mysql.password=.*$/spring.datasource.hikari.data-sources[0].mysql.password=$CRUDAPI_MYSQL_PASSWORD/g" application-prod.properties
+sed -i "s/spring.datasource.hikari.data-sources.*.mysql.password=.*$/spring.datasource.hikari.data-sources[0].mysql.password=$CRUDAPI_MYSQL_PASSWORD/g" application-prod.properties
 
 if [ "$DB_ACTION" == "INIT_DATA" ]; then
     echo "[dev]DB_ACTION INIT_DATA..."
