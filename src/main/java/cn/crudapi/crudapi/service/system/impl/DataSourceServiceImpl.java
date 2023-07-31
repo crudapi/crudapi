d9fbcccd25d1ce2a8c896653d162fd6608c3bee9
crudapi <admin@crudapi.cn>
2023-07-31 18:06:03 +0800

    enhance sql

--- src/main/java/cn/crudapi/crudapi/service/system/impl/DataSourceServiceImpl.java
+++ src/main/java/cn/crudapi/crudapi/service/system/impl/DataSourceServiceImpl.java
@@ -4,17 +4,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.crudapi.crudapi.config.datasource.DynamicDataSourceProperties;
 import cn.crudapi.crudapi.config.datasource.DynamicDataSourceProvider;
+import cn.crudapi.crudapi.constant.ColumnConsts;
+import cn.crudapi.crudapi.constant.SqlConsts;
+import cn.crudapi.crudapi.constant.Status;
 import cn.crudapi.crudapi.constant.SystemConsts;
 import cn.crudapi.crudapi.service.CrudService;
 import cn.crudapi.crudapi.service.system.DataSourceService;
 
 @Service
 public class DataSourceServiceImpl implements DataSourceService {
+	private static final Logger log = LoggerFactory.getLogger(ConfigServiceImpl.class);
+
 	@Autowired
 	private CrudService crudService;
 	
@@ -23,7 +30,20 @@ public class DataSourceServiceImpl implements DataSourceService {
 
 	@Override
 	public List<Map<String, Object>> list() {
-        return crudService.queryForList("SELECT * FROM `" + SystemConsts.TABLE_DATA_SOURCE +  "`", new HashMap<String, Object>());
+		Map<String, Object> paramsMap = new HashMap<String, Object>();
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(SqlConsts.SELECT)
+			.append(" * ")
+			.append(SqlConsts.FROM)
+			.append(" ")
+			.append(SystemConsts.TABLE_DATA_SOURCE)
+			.append(" ");
+		
+		String sql = sb.toString();
+		
+		log.info(sql);
+        return crudService.queryForList(sql, paramsMap);
     }
 
 	@Override
