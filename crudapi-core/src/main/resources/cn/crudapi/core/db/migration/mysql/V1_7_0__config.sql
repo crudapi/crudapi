e852418d1cb78477f8e9121f637703d85e3a36dc
crudapi <admin@crudapi.cn>
2024-07-25 13:21:57 +0800

    config

---
+++ crudapi-core/src/main/resources/cn/crudapi/core/db/migration/mysql/V1_7_0__config.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ca_config` (`name`, `key`, `value`, `fullTextBody`, `createdDate`, `lastModifiedDate`, `createById`, `updateById`, `ownerId`, `isDeleted`) VALUES
+('隐藏官网', 'toolBarHiddenOfficialWebsite', 'false', '应用名称 toolBarHiddenOfficialWebsite false 1 1 1 false', '2024-07-23 16:56:28', NULL, 1, 1, 1, b'0'),
+('隐藏帮助', 'toolBarHiddenHelp', 'false', '业务数据名称 toolBarHiddenHelp false 1 1 1 false', '2024-07-23 16:57:20', NULL, 1, 1, 1, b'0'),
+('隐藏源码', 'toolBarHiddenCode', 'false', '业务数据名称 toolBarHiddenCode false 1 1 1 false', '2024-07-23 16:57:20', NULL, 1, 1, 1, b'0');
+COMMIT;
