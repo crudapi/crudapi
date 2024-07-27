a25bf9354a3773d445f4a17b9f88c5ece5e2a66d
crudapi <admin@crudapi.cn>
2024-07-27 12:02:27 +0800

    sql

---
+++ crudapi-core/src/main/resources/cn/crudapi/core/db/migration/mysql/V1_8_0__menu.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ca_menu` (`name`, `code`, `fullTextBody`, `createdDate`, `lastModifiedDate`, `createById`, `updateById`, `ownerId`, `isDeleted`) VALUES
+('系统', 'system', '系统 system 1 1 1 false', '2024-07-24 12:48:42', NULL, 1, 1, 1, b'0');
+COMMIT;
